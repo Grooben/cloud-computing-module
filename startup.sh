decc335438a73a9219201344e0c756fe502fff8f
BuildTools <unconfigured@null.spigotmc.org>
2019-12-03 20:38:26 +0000

    updated ubuntu, also actually allowed SSH this time...

--- startup.sh
+++ startup.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-sudo su -
-apt update
-apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli phpmyadmin php-mbstring php-gettext
-chmod -R 0755 /var/www/html/
-systemctl enable apache2
-systemctl start apache2
-phpenmod mcrypt
-phpenmod mbstring
-systemctl restart apache2
-echo "<?php phpinfo(); ?>" > /var/www/html/index.php 
+sudo apt update
+sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli phpmyadmin php-mbstring php-gettext
+sudo chmod -R 0755 /var/www/html/
+sudo systemctl enable apache2
+sudo systemctl start apache2
+sudo phpenmod mcrypt
+sudo phpenmod mbstring
+sudo systemctl restart apache2
+sudo echo "<?php phpinfo(); ?>" > /var/www/html/index.php 
