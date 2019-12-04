ea00da59b8ab4a69343a3f0e5f0d3d7d6b803c82
OhGrooben <43583016+OhGrooben@users.noreply.github.com>
2019-12-04 16:23:31 +0000

    -y to the rescue!

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo apt update
-sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli phpmyadmin php-mbstring php-gettext
+sudo apt -y install apache2 mysql-server php php-mysql libapache2-mod-php php-cli phpmyadmin php-mbstring php-gettext
 sudo chmod -R 0755 /var/www/html/
 sudo systemctl enable apache2
 sudo systemctl start apache2
